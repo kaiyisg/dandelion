451fcffef726d98fef6cf610643ee8c25672a4f6
jeremy.hon <jeremy.hon.gy@gmail.com>
2017-02-18 20:06:14 -0800

    add index.html

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.get('/', functio(req, res){
-  res.send('Waiting for Signal')
+app.get('/', function(req, res){
+  res.sendFile('public/index.html');
 })
 
 app.listen(3000, function() {
