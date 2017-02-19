811a11cad8d11eb81c87f32597a266a2743cdc40
jeremy.hon <jeremy.hon.gy@gmail.com>
2017-02-18 20:28:07 -0800

    add abs path

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-  res.sendFile('public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(3000, function() {
