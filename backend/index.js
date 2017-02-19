65e2f9f75958819603818c23630cbf9bc98d2317
jeremy.hon <jeremy.hon.gy@gmail.com>
2017-02-18 20:38:30 -0800

    add mongo code

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,34 @@
 var express = require('express')
 var app = express()
+var assert = require('assert')
+
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+
+var mongoStr = 'mongodb://dandelion:jhhuhKgY6FEZHQ7l1VECRknnftnO55KX0mdbIiO2yASI89BuWn4SQORzulwM3809WqXQyEBjYWc2J1XhlCXgvw==@dandelion.documents.azure.com:10250/?ssl=true/dandelion_db';
+
+var insertDocuments = function(db, callback) {
+  var collection = db.collection('test_events');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  })
+}
+
+MongoClient.connect(mongoStr, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
